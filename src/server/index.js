8bc7a87ddeeeee7993f45bca5964f75d2b5978e6
v_bonchevski <v.bonchevski@valteodigital.com>
2019-09-30 12:18:53 +0300

    Commiting all changes to work from a diff machine

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import compress from 'compression';
+import services from './services';
 const app = express();
 import path from 'path';
 
@@ -33,4 +34,17 @@ app.use('/uploads', express.static(path.join(root, 'uploads')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(root, '/dist/client/index.html'));
 });
+
+// Initiate Services 
+const serviceNames = Object.keys(services);
+
+for (let i = 0; i < serviceNames.length; i += 1) {
+  const name = serviceNames[i];
+  if (name === 'graphql') {
+    services[name].applyMiddleware({ app });
+  } else {
+    app.use(`/${name}`, services[name]);
+  }
+}
+
 app.listen(8000, () => console.log('Listening on port 8000!'));
